c956071bb9a76c98e531aaf4c6e8b6203037f97b
Oksana <osushinskaya@yaypay.com>
2022-02-07 22:51:18 +0200

    Fix taks 04-create-getter

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,6 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (typeof obj === 'undefined') return;
+  return Object.assign({}, ...Object.entries(obj).map(([key,value]) => ({ [value]: key })))
 }
